d1177d466dfd82c2ada143786ab5a47ab94a4488
ellyandrew <ellyadriano12@gmail.com>
2025-12-10 15:48:11 +0300

    DB Update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,22 @@
 const mysql = require('mysql2/promise');
 
+const fs = require('fs');
+
 require('dotenv').config();
 
+// Local
+// const db = mysql.createPool({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE,
+//   port: process.env.DATABASE_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// Production
 const db = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -10,7 +25,10 @@ const db = mysql.createPool({
   port: process.env.DATABASE_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    ca: fs.readFileSync(__dirname + '/BaltimoreCyberTrustRoot.crt.pem')
+  }
 });
 
 module.exports = db;
